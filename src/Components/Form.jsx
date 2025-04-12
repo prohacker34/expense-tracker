e4fe743e2fbea23e4d655cf267eaf6f9ee48ab64
Habert <habertotieno4@gmail.com>
2025-04-12 12:41:01 +0300

    added a calendar dropdown menu

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import Table from './Table'
 
+
+
+
+function handleSubmit(event) {
+
+  event.preventDefault()
+
+
+
+}
 function Form() {
-  function handleSubmit(event) {
-    event.preventDefault()
-    event.reset()
-  }
+
   function handleClick() {
-   alert('submited')
+
   }
   return (
     <div id='form-submit'>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}Submit>
             <h1>Add Expense</h1>
             <p>Enter your details below</p>
-            <input type="text"    placeholder='Enter expense name'className='form-input'/><br />
-            <input type="text"   placeholder='Enter expense description'className='form-input'/><br />
-            <input type="text"   placeholder='Enter expense category'className='form-input'/><br />
-            <input type="text"   placeholder='Enter amount'className='form-input'/><br />
-            <input type="text"   placeholder='dd/mm/yr'className='form-input'/><br />
+            <input type="text"     placeholder='Enter expense name'className='form-input'/><br />
+            <input type="text"    placeholder='Enter expense description'className='form-input'/><br />
+            <input type="text"    placeholder='Enter expense category'className='form-input'/><br />
+            <input type="text"    placeholder='Enter amount'className='form-input'/><br />
+            <input type="date" value="dd/mm/yr"placeholder='dd/mm/yr'className='form-input'/><br />
             <button type='submit' id='button'onClick={handleClick}>submit</button>
         </form>
 
