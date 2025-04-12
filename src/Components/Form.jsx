f5264ba28841633d9554eaaafc81ff3002cec483
Habert <habertotieno4@gmail.com>
2025-04-12 10:34:00 +0300

    added event listeners

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import Table from './Table'
 
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault()
+    event.reset()
+  }
+  function handleClick() {
+   alert('submited')
+  }
   return (
     <div id='form-submit'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Add Expense</h1>
             <p>Enter your details below</p>
-            <input type="text"  className='form-input'/><br />
-            <input type="text"  className='form-input'/><br />
-            <input type="text"  className='form-input'/><br />
-            <input type="text"  className='form-input'/><br />
-            <input type="text"  className='form-input'/><br />
-            <button type='submit' id='button'>submit</button>
+            <input type="text"    placeholder='Enter expense name'className='form-input'/><br />
+            <input type="text"   placeholder='Enter expense description'className='form-input'/><br />
+            <input type="text"   placeholder='Enter expense category'className='form-input'/><br />
+            <input type="text"   placeholder='Enter amount'className='form-input'/><br />
+            <input type="text"   placeholder='dd/mm/yr'className='form-input'/><br />
+            <button type='submit' id='button'onClick={handleClick}>submit</button>
         </form>
 
         <Table />
